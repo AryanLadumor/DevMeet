67d873452db6a7f1ecde97a72386f6341afb9ca5
AryanLadumor <aryanladumor666@gmail.com>
2026-04-04 21:47:05 +0530

    feat: implement routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./components/layout/Body";
+import Auth from "./components/Auth/Auth";
+import Profile from "./components/profile/Profile";
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
