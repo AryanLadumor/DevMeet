4d1895fab5c4fc9b4094e760d1fe37c2c125b410
AryanLadumor <aryanladumor666@gmail.com>
2026-04-12 17:51:40 +0530

    fix: fix cors options

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,43 +1,46 @@
-require("dotenv").config()
-const port = process.env.PORT
-const ip = process.env.PUBLIC_IP
-const express = require("express")
-var cors = require('cors')
+require("dotenv").config();
+const port = process.env.PORT;
+const ip = process.env.PUBLIC_IP;
+const express = require("express");
+var cors = require("cors");
 const app = express(); //app --> Server
 //configs
-const connectDB = require("./config/database.js")
+const connectDB = require("./config/database.js");
 //npm pakages
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 //Routers
 const authRouter = require("./routes/auth.routes.js");
 const profileRouter = require("./routes/profile.routes.js");
 const requestRouter = require("./routes/request.routes.js");
 const userRouter = require("./routes/user.routes.js");
 //middleWares
-const  corsOptions = {
-      origin: `http://${ip}/${port}`,  // whitelist the frontend part so cokies can be stored automatically
-      credentials: true,
-    };
-app.use(cors(corsOptions))
+// backend/src/app.js
+const corsOptions = {
+  // Allow the Nginx frontend (Port 80) and the direct backend port (7777)
+  origin: [`http://${ip}`, `http://${ip}:7777`],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/" , authRouter);
-app.use("/" , profileRouter);
-app.use("/" , requestRouter);
-app.use("/" , userRouter);
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+app.use("/", userRouter);
 
+app.use("/", (err, req, res) => {
+  console.log(err);
+}); //Error middleware to handle any Error
 
- app.use("/" , (err,req,res)=>{
-    console.log(err)
- }) //Error middleware to handle any Error
-
-
-connectDB().then(() => {
+connectDB()
+  .then(() => {
     console.log("MongoDB server is Connect");
-    app.listen(port, () => { //it takes port to listen requets and cb which is called when server is running
-        console.log(`server running on http://${ip}:${port}`)
-    });  // listing the request here to make sure that --> 1st mongo service is starded 
-}).catch((err) => {
-    console.log(err)
-})
+    app.listen(port, () => {
+      //it takes port to listen requets and cb which is called when server is running
+      console.log(`server running on http://${ip}:${port}`);
+    }); // listing the request here to make sure that --> 1st mongo service is starded
+  })
+  .catch((err) => {
+    console.log(err);
+  });
